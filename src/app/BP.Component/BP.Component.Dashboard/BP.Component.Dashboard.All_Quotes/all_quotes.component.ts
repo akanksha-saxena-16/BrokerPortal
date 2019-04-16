03ad99a0325a496998d23b63953ca9dc7056c018
akanksha-saxena-16 <thecodeobfuscator@gmail.com>
2019-04-16 12:54:50 +0530

    component update

--- src/app/BP.Component/BP.Component.Dashboard/BP.Component.Dashboard.All_Quotes/all_quotes.component.ts
+++ src/app/BP.Component/BP.Component.Dashboard/BP.Component.Dashboard.All_Quotes/all_quotes.component.ts
@@ -1,10 +1,7 @@
-<div class="home-usp home-usp-1">
-    <div class="usp-icon">
-        <img src="assets/images/all-bound-policy.png">
-    </div>
-    <div class="usp-content">
-        <h3 class="usp-title">86</h3>
-        <p class="usp-description" title="All Quotes">All Quotes</p>
-        <p class="date-from"> <i class="icon-calendar3"></i> 01/01/2018 to 31/12/2018 </p>
-    </div>
-</div>
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-all-quotes',
+  templateUrl: './all_quotes.component.html'
+})
+export class AllQuotesComponent {}
